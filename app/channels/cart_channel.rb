89d1546914ea944d7f2efd702af8150063c325ba
Washington <cardosowashington2003@gmail.com>
2024-02-07 17:34:57 -0300

    Update the project

--- app/channels/cart_channel.rb
+++ app/channels/cart_channel.rb
@@ -1,9 +1,14 @@
 class CartChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "cart_channel_#{current_user.id}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+  end
+
+  def broadcast_update
+    ActionCable.server.broadcast("cart_channel_#{current_user.id}", { action: 'update_cart' })
   end
 end
